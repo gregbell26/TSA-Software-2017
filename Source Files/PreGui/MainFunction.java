474567aef051bad44ee6b2ebbf29273538c8b438
TriHard Studios <trihard.studios@outlook.com>
2017-09-20 16:07:54 -0600

    Defined the main function

--- Source Files/PreGui/MainFunction.java
+++ Source Files/PreGui/MainFunction.java
@@ -1,5 +1,17 @@
 package PreGui;
 
+/**
+ * Bugs:
+ * None for now...
+ *
+ * Solutions:
+ * Who Knows....
+ *
+ *
+ * Thing ToDo:
+ * Everything
+ *
+ */
 
 public class MainFunction{
     public static void main(String[] args){
