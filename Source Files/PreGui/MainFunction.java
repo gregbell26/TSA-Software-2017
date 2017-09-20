75511b5778ce5bc979643939b6e2e73322a53fd6
TriHard Studios <trihard.studios@outlook.com>
2017-09-20 15:58:46 -0600

    Defined the main function

---
+++ Source Files/PreGui/MainFunction.java
@@ -0,0 +1,8 @@
+package PreGui;
+
+
+public class MainFunction{
+    public static void main(String[] args){
+
+    }
+}
--- Source Files/Test.java
+++
@@ -1,6 +0,0 @@
-public class Test {
-
-    public static void main(String[] agrs){
-        System.out.println("Hello There... You should give me a purpose");
-    }
-}
