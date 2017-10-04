5751a00b27ba28d7f335b57f62e606edc4d7c8c9
TriHard Studios <trihard.studios@outlook.com>
2017-10-04 16:06:30 -0600

    .

--- Source Files/Backend/Encrypter.java
+++ Source Files/Backend/Encrypter.java
@@ -6,6 +6,21 @@ package Backend;
 //some forms of encrytion that we should look into is AES - 128, 256 we should also look into DES or 3DES as they are very secure. RSA also might be good to look into as well.
 //This will need a variable called encryer out.
 public class Encrypter {
+
+
+    private String text;
+    public Encrypter(){
+        //TODO ADD CODE
+    }
+
+    public Encrypter(String userInput){
+
+    }
+
+    public String textOutput(){
+        return text;
+    }
+
 }
 
 
