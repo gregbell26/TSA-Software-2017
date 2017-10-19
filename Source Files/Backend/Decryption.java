1630e6e1cde8e9fe507d17fa6521a200d76c8503
TriHard Studios <trihard.studios@outlook.com>
2017-10-18 20:12:40 -0600

    IT WORKS!!!!!!!

--- Source Files/Backend/Decryption.java
+++ Source Files/Backend/Decryption.java
@@ -5,40 +5,36 @@ public class Decryption  {
 
     int key = Encryption.key;
     public static String text = "";
-    String decryptedText;
+    static String decryptedText;
     static String decryptMethod;
     public Decryption(){//default constructor
         decryptedText = Encryption.encryptedText;
         decryptMethod = Encryption.encryptMethod;
     }
 
-    //encryption methods
+    //decryption methods
     private String decryptCaesar() {
-        key = key * -1;
         String text = decryptedText;
         int n = text.length();
         String output = "";
         for (int i = 0; i < n; i++) {
             int a = (int) text.charAt(i);
-            if ((a >= 65) && (a <= 90)) {
-                a = Character.toLowerCase(a);
-            }
+
             if ((a >= 97) && (a <= 122)) {
-                a -= 96;
-                a += key;
+                a = a - (key % 26);
                 char c = (char) ((a % 26) + 96);
-                output += c;
-            } else {
-                output += (char)a;
+                if (a < 'a') {
+                    a = a + 26;
+                }
+                //output += c;
             }
+            output += (char) a;
         }
         return output;
     }
-
+//master decryption
     public String decrypt(){
-        //System.out.println("Hi there encryper ");
         if(decryptMethod.equals("caesar")){
-            //System.out.println("Hi there");
             decryptedText = decryptCaesar();
         }
         return decryptedText;
